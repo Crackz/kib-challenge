aa14d916fa4ff9534c3a708965616f077c9a6588
Magdy <vipa00magdy00@gmail.com>
2024-10-09 20:44:45 +0200

    Changed cache manager to be able to read redis configuration from config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,11 +8,11 @@ import { EnvironmentVariables } from './common/env/environment-variables';
 import { validateEnvironmentVariables } from './common/env/validation';
 import { envFilePaths } from './config';
 import { typeormConfig } from './config/typeorm';
-import { MoviesModule } from './modules/movies/movies.module';
-import { CacheModule } from '@nestjs/cache-manager';
-import { UsersModule } from './modules/users/users.module';
-import { MoviesRatingsModule } from './modules/movies-ratings/movies-ratings.module';
 import { MoviesFavoritesModule } from './modules/movies-favorites/movies-favorites.module';
+import { MoviesRatingsModule } from './modules/movies-ratings/movies-ratings.module';
+import { MoviesModule } from './modules/movies/movies.module';
+import { UsersModule } from './modules/users/users.module';
+import { RedisStore, redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -27,12 +28,26 @@ import { MoviesFavoritesModule } from './modules/movies-favorites/movies-favorit
         return configService.get('typeorm');
       },
     }),
-    CacheModule.register({
+    CacheModule.registerAsync({
       isGlobal: true,
-      // !Note:
-      // find movies response would take on average 66 kb with a limit of 1000 cached response
-      // it would take on around 66 mb to cache 1000 find movies responses
-      max: 1000,
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService<EnvironmentVariables>,
+      ) => {
+        const store = await redisStore({
+          socket: {
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
+          },
+        });
+        return {
+          store: store as RedisStore & CacheStore,
+          // !Note:
+          // find movies response would take on average 66 kb with a limit of 1000 cached response
+          // it would take on around 66 mb to cache 1000 find movies responses
+          max: 1000,
+        };
+      },
     }),
     UsersModule,
     MoviesModule,
